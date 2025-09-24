09d668e36d7e08a32d13cd8b4504e0d6cbf0ab12
Yoann BLOSSIER <yoann.blossier@toenn-vaot.fr>
2025-09-25 00:17:32 +0200

    build: Change the IStateRecord composition to handle minimum audit case docs: change version to 1.0.8

--- IFS_DEV/IFS_SRC/ToennVaot.Components.Core/ToennVaot.Components.Core.Models.Interfaces/IStateRecord.cs
+++ IFS_DEV/IFS_SRC/ToennVaot.Components.Core/ToennVaot.Components.Core.Models.Interfaces/IStateRecord.cs
@@ -3,26 +3,12 @@
 /// <summary>
 /// The base interface for state records
 /// </summary>
-public interface IStateRecord
+public interface IStateRecord<TUser>
 {
     /// <summary>
     /// The status identifier
     /// </summary>
     public int StatusId { get; set; }
-}
-
-/// <summary>
-/// Interface defines a state record
-/// </summary>
-/// <typeparam name="TState">The enumeration of states to apply</typeparam>
-/// <typeparam name="TUser">The type to use to designate the user operating the actions</typeparam>
-public interface IStateRecord<TState, TUser> : IStateRecord
-    where TState : struct, Enum
-{
-    /// <summary>
-    /// The status identifier
-    /// </summary>
-    public TState Status { get; set; }
 
     /// <summary>
     /// The date and time when the status was last changed
@@ -33,4 +19,18 @@ public interface IStateRecord<TState, TUser> : IStateRecord
     /// The user who last changed the status
     /// </summary>
     public TUser StateChangedBy { get; set; }
+}
+
+/// <summary>
+/// Interface defines a state record
+/// </summary>
+/// <typeparam name="TState">The enumeration of states to apply</typeparam>
+/// <typeparam name="TUser">The type to use to designate the user operating the actions</typeparam>
+public interface IStateRecord<TState, TUser> : IStateRecord<TUser>
+    where TState : struct, Enum
+{
+    /// <summary>
+    /// The status identifier
+    /// </summary>
+    public TState Status { get; set; }
 }
