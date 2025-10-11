6b5070598135033898ffbdb90d243c059cfda090
Yoann BLOSSIER <yoann.blossier@toenn-vaot.fr>
2025-10-11 18:48:13 +0200

    build: Add new operation filter to manage per tenant usage

---
+++ IFS_DEV/IFS_SRC/ToennVaot.Components.Core/ToennVaot.Components.Core.Api/Filters/Swagger/SwaggerRequiredTenantHeaderParameterOperationFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ToennVaot.Components.Core.Api.Filters.Swagger
+{
+    /// <summary>
+    /// Operation filter to the "X-Tenant-Id" header parameter to all API operations in Swagger documentation.
+    /// </summary>
+    public class SwaggerRequiredTenantHeaderParameterOperationFilter : IOperationFilter
+    {
+        /// <inheritdoc />
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+            {
+                operation.Parameters = new List<OpenApiParameter>();
+            }
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "X-Tenant-Id",
+                In = ParameterLocation.Header,
+                Required = true,
+                Description = "The tenant identifier",
+                Schema = new OpenApiSchema
+                {
+                    Type = "string",
+                    Format = "uuid"
+                }
+            });
+        }
+    }
+}
